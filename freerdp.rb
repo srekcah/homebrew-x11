211ee495b7e3784e5a925e893c97779ad3e7fc99
esrever10 <esrever10@outlook.com>
2015-10-08 14:54:13 +0800

    Update freerdp.rb

--- freerdp.rb
+++ freerdp.rb
@@ -2,24 +2,8 @@ class Freerdp < Formula
   homepage "http://www.freerdp.com/"
 
   stable do
-    url "https://github.com/FreeRDP/FreeRDP/archive/1.0.2.tar.gz"
-    sha256 "c0f137df7ab6fb76d7e7d316ae4e0ca6caf356e5bc0b5dadbdfadea5db992df1"
-
-    patch do
-      url "https://github.com/FreeRDP/FreeRDP/commit/1d3289.diff"
-      sha256 "90a4f288349dd13b658b3401762373f0dec8414233bf304df0715724c8352cec"
-    end
-
-    patch do
-      url "https://github.com/FreeRDP/FreeRDP/commit/e32f9e.diff"
-      sha256 "1b8ec9d5e229b4ed50790042bee7ceb3e85bdd88c9bc2dcfdf2e9d97e921649d"
-    end
-
-    # https://github.com/FreeRDP/FreeRDP/pull/1682/files
-    patch do
-      url "https://gist.githubusercontent.com/bmiklautz/8832375/raw/ac77b61185d11aa69e5f6b5e88c0fa597c04d964/freerdp-1.0.2-osxversion-patch.diff"
-      sha256 "2e8f68a0dbe6e2574dec3353e65a4f03d76a3398f8fac536fda08c24748aec2b"
-    end
+    url "https://github.com/FreeRDP/FreeRDP/archive/1.1.0-beta1.tar.gz"
+    sha256 "c7c7dd6efdc8bbb8e6c545216391badf92776ea7d724c79aaed8f78dbc3ab94e"
   end
 
   bottle do
